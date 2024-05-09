adef394cf45676af57ed2cba8b5d23ee94332ed7
Billycan1972 <waldo.griffiths@gmail.com>
2024-05-09 14:30:20 +0000

    9demayorec

--- src/workflow-01/fe_intranames/wgpc501_workflow_intrames_fe_iter1.r
+++ src/workflow-01/fe_intranames/wgpc501_workflow_intrames_fe_iter1.r
@@ -11,7 +11,7 @@ require("ParamHelpers")
 envg <- env()
 
 envg$EXPENV <- list()
-envg$EXPENV$exp_dir <- "~/buckets/b1/exp/"
+envg$EXPENV$exp_dir <- "~/buckets/b1/exp7/"
 envg$EXPENV$wf_dir <- "~/buckets/b1/flow/"
 envg$EXPENV$wf_dir_local <- "~/flow/"
 envg$EXPENV$repo_dir <- "~/labo2024v1/"
